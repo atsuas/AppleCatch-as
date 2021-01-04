4d9686856c4bb3eb8b0f5f18f3dc428754c87cf7
as <>
2021-01-05 03:24:16 +0900

    アイテムを落下させるスクリプト

--- Assets/Script/ItemController.cs
+++ Assets/Script/ItemController.cs
@@ -4,15 +4,15 @@ using UnityEngine;
 
 public class ItemController : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float dropSpeed = -0.03f;   
 
     // Update is called once per frame
     void Update()
     {
-        
+        transform.Translate(0, this.dropSpeed, 0);
+        if (transform.position.y < -1.0f)
+        {
+            Destroy(gameObject);
+        }
     }
 }
