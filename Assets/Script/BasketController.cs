608db479cf18d13c02e54c1cf048383e9ccd2406
as <>
2021-01-05 04:29:05 +0900

    キャッチした際のAudio設定・appleとbombのPrefabを作成

--- Assets/Script/BasketController.cs
+++ Assets/Script/BasketController.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class BasketController : MonoBehaviour
 {
-    private void OnTriggerEnter(Collider other)
+    public AudioClip appleSE;
+    public AudioClip bombSE;
+    AudioSource aud;
+
+    void Start()
+    {
+        this.aud = GetComponent<AudioSource>();
+    }
+
+    void OnTriggerEnter(Collider other)
     {
         if (other.gameObject.tag == "Apple")
         {
-            Debug.Log("Tag=Apple");
+            this.aud.PlayOneShot(this.appleSE);
         }
         else
         {
-            Debug.Log("Tag=Bomb");
+            this.aud.PlayOneShot(this.bombSE);
         }
         Destroy(other.gameObject);
     }
